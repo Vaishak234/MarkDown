32227d9341d6bdba2aeae8fddad2083d6a792cd3
vaishakhp <vaishakhpat2003@gmail.com>
2023-08-16 16:41:34 +0530

    initial3 commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 
 export default axios.create({
-    baseURL: 'https://markdown-33vv.onrender.com/api',
+    baseURL: 'https://markdown-33vv.onrender.com',
     headers: { 'Content-Type': 'application/json' }
 })
