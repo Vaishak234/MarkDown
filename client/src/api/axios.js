a0791eb54f1441f2ec96474b69f54805e7562994
Vaishak234 <102749929+Vaishak234@users.noreply.github.com>
2023-08-17 13:44:21 +0530

    Update axios.js 2

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,5 +3,6 @@ import axios from 'axios'
 
 export default axios.create({
     baseURL: 'https://markdown-398n.onrender.com/api',
-    headers: { 'Content-Type': 'application/json' }
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials:true
 })
